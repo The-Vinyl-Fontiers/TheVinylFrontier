b50a35b3ea1ae04ad6d64f8f87fc650f9bece976
David Kapaku <dkapaku1@vt.edu>
2023-03-28 17:18:48 -0400

    index

---
+++ frontend/src/components/index.js
@@ -0,0 +1,13 @@
+export {default as Homepage} from "./Homepage";
+export {default as Login} from "./Login"
+export {default as Profile} from "./Profile"
+export {default as Register} from "./Register"
+export {default as AllProducts} from "./AllProducts"
+export {default as Cart} from "./Cart"
+export {default as Checkout} from "./Checkout"
+export {default as Header} from "./Header"
+export {default as Logout} from "./Logout"
+export {default as Navbar} from "./Navbar"
+export {default as OrderHistory} from "./OrderHistory"
+export {default as PaymentScreen} from "./PaymentScreen"
+export {default as SingleProduct} from "./SingleProduct"
