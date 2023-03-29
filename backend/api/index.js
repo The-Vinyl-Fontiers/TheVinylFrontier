d8a6ad8d80700c02f557ebcf184fc3d65536e354
cforlini24 <cforlini24@gmail.com>
2023-03-29 16:39:48 -0400

    working on api/vinyls

--- backend/api/index.js
+++ backend/api/index.js
@@ -19,8 +19,8 @@ router.use('/orders', ordersRouter);
 const vinylRouter = require('./vinyl');
 router.use('/vinyl', vinylRouter);
 
-const paymentsRouter = require('./Payments');
-router.use('/Payments', paymentsRouter);
+// const paymentsRouter = require('./Payments');
+// router.use('/Payments', paymentsRouter);
 
 const orderProductsRouter = require('./order_products');
 router.use('/order_products', orderProductsRouter);
