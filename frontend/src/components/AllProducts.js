8c35534ff3b5ddc4d00fdcffd81ea20e2e0b486a
David Kapaku <dkapaku1@vt.edu>
2023-03-30 12:53:02 -0400

    fixes

--- frontend/src/components/AllProducts.js
+++ frontend/src/components/AllProducts.js
@@ -2,12 +2,19 @@ import { useEffect } from "react";
 
 const AllProducts=(props)=>{
     const{setLoggedIn,vinyls,fetchVinyls}=props;
-    useEffect(()=>{
-        if(localStorage.getItem("token")){
-            setLoggedIn(true);
-            fetchVinyls();
-        }
-    })
+    try{
+        const fetchData=async () =>{
+        try {response = await fetch('http://localhost:3000:/',{
+            header: {
+                'Content-Type':'application/json',
+            }
+        });
+        const {data} = await response.json();
+        return data.posts
+            
+        }catch(error){
+            alert ("error fetching posts")}
+    }}
 //INCOMPLETE
 }
 export default AllProducts
