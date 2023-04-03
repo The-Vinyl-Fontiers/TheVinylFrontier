84b9e80bb907e0621ce4532822b217fcb8124bcd
cforlini24 <cforlini24@gmail.com>
2023-04-03 15:03:05 -0400

    linked to single product comp

--- src/components/AllProducts.js
+++ src/components/AllProducts.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { Link } from "react-router-dom";
 
 const AllProducts = (props) =>{
     const {vinyls} = props
@@ -10,13 +11,13 @@ const AllProducts = (props) =>{
                 {
                     vinyls ? vinyls.map((vinyl) =>{
                         return(
-                            <div className="singleItem"key={vinyl.id}>
+                            <Link to={`/vinyl/${vinyl.id}`} className="singleItem"key={vinyl.id}>
                                 <p>{vinyl.title}</p>
                                 <p>{vinyl.artist}</p>
                                 <p>${vinyl.price}</p>
                                 <p>{vinyl.yearReleased}</p>
                                 <img src={`${vinyl.imgURL}`} className="vinylImg"/>
-                            </div>
+                            </Link>
                         )
                     }) : "No data loaded"
                 }
