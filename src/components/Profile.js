27070a6240f1f6ede6693c07a114d0c9a5220539
arjuna1011 <jgboatright@att.net>
2023-04-06 12:35:33 -0400

    basic profile component

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -2,6 +2,9 @@ import { useEffect, useState } from "react";
 
 const Profile = (props) => {
     const [myData, setMyData] = useState({})
+    const [updateUsername, setUpdateUsername] = useState("");
+    const [updatePassword, setUpdatePassword] = useState("");
+    const [updateEmail, setUpdateEmail] = useState("");
 
     useEffect(() => {
         console.log(localStorage.getItem("token"))
@@ -17,7 +20,7 @@ const Profile = (props) => {
         async function fetchMyData() {
             try {
 
-                const response = await fetch("http://localhost:3000/users/:username", {
+                const response = await fetch("http://localhost:3000/users/me", {
                     headers: {
                         "Content-Type": "application/json",
                         'Authorization': `Bearer ${localStorage.getItem("token")}`
@@ -34,20 +37,47 @@ const Profile = (props) => {
                 console.log(error);
             }
         }
+
+        const updateUser = async (userID) => {
+            console.log("userID", userID)
+            try {
+                const response = await fetch(`http://localhost:3000/users/${username}`, {
+                    method: "PATCH",
+                    headers: {
+                        'Content-Type': 'application/json',
+                        'Authorization': `Bearer ${myJWT}`
+                    },
+                    body: JSON.stringify({
+                        username: updateUsername,
+                        password: updateDescription,
+                        email: updateEmail
+                    })
+                });
+
+                const translatedData = await response.json();
+                console.log(translatedData);
+                if (translatedData) {
+                    setMyData(translatedData)
+                }
+            } catch (error) {
+                console.log(error);
+            }
+        }
+        function updateButton(event) {
+            console.log("Button is clicked");
+            updateUser(event.target.value)
+        };
     }, [])
 
     return (
         <div>
-            {
-                props.isLoggedIn ?
-                    <div>
-                        <h3>Welcome, {myData.username}</h3>
-                        <h3>Here are your orders: {myData.orders}</h3>
-                    </div>
-                    :
-                    <h3>Please login or register for a new account.</h3>
-
-            }
+            {/* Render the profile data here */}
+            <div>
+                <input type="text" value={updateUsername} onChange={(event) => setUpdateUsername(event.target.value)} />
+                <input type="text" value={updateEmail} onChange={(event) => setUpdateEmail(event.target.value)} />
+                <input type="password" value={updatePassword} onChange={(event) => setUpdatePassword(event.target.value)} />
+                <button value={myData.id} onClick={updateButton}>Update</button>
+            </div>
         </div>
     )
 }
