8ff69316e78907640f898c2fec0087398ad1e33a
arjuna1011 <jgboatright@att.net>
2023-04-06 15:39:37 -0400

    2nd draft profile

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 
 const Profile = (props) => {
     const {currentUser, setCurrentUser, loggedIn, setLoggedIn} = props;
-    const [updateUsername, setUpdateUsername] = useState("");
+    const [updateUsername, setUpdateUsername] = useState(currentUser.username);
     const [updatePassword, setUpdatePassword] = useState("");
     const [updateEmail, setUpdateEmail] = useState("");
 
@@ -58,24 +58,24 @@ const Profile = (props) => {
         return (
             <div>
                 {/* Render the profile data here */}
-                
-                    // loggedIn ?
+                {
+                    props.loggedIn ?
                         <div>
                             <h3>Welcome, {currentUser.username}</h3>
     
                             <form onSubmit={(event) => {event.preventDefault() 
                                 updateUser(currentUser.id)}}>
-                                <input type="text" value={updateUsername} onChange={(event) => setUpdateUsername(event.target.value)} />
-                                <input type="text" value={updateEmail} onChange={(event) => setUpdateEmail(event.target.value)} />
-                                <input type="password" value={updatePassword} onChange={(event) => setUpdatePassword(event.target.value)} />
+                                <input type="text" value={updateUsername} placeholder= {`${currentUser.username}`} onChange={(event) => setUpdateUsername(event.target.value)} />
+                                <input type="text" value={updateEmail} placeholder= "new email" onChange={(event) => setUpdateEmail(event.target.value)} />
+                                <input type="password" value={updatePassword}  placeholder= "new password" onChange={(event) => setUpdatePassword(event.target.value)} />
                                 <button type="submit">Update</button>
                             </form>
                         </div>
     
-                        
+                        :
                         <h3>Please login or register for a new account!</h3>
     
-                
+                }
             </div>
         )
     }
