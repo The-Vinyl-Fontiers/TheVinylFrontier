74ed3831209d01c8f6da7cfdedd1e925b88ddea6
arjuna1011 <jgboatright@att.net>
2023-04-10 12:20:57 -0400

    rough draft for checkout component

--- src/components/Checkout.js
+++ src/components/Checkout.js
@@ -1,34 +1,124 @@
-import React from 'react';
-import { useStripe } from '@stripe/react-stripe-js';
+import { useState } from "react";
 
-const StripeCheckoutButton = ({ cartSubtotal, cartTaxTotal }) => {
-    const stripe = useStripe();
+const Checkout = (props) => {
+    const { cart } = props
+    const [formData, setFormData] = useState({
+        cardNumber: "",
+        cardName: "",
+        expiration: "",
+        cvv: "",
+        address: "",
+        city: "",
+        state: "",
+        zip: ""
+    });
 
-    const handleClick = async (event) => {
-        // Call your backend to create a Stripe Checkout Session
-        const response = await fetch('/api/create-checkout-session', {
-            method: 'POST',
-            body: JSON.stringify({
-                amount: (cartSubtotal + cartTaxTotal) * 100, // Stripe expects amount in cents
-                // Add any additional information here that you need to pass to your backend
-            }),
-            headers: {
-                'Content-Type': 'application/json',
-            },
-        });
-        const { sessionId } = await response.json();
+    const handleFormChange = (event) => {
+        const { name, value } = event.target;
+        setFormData((prevState) => ({ ...prevState, [name]: value }));
+    };
 
-        // Redirect the user to the Stripe Checkout page
-        const result = await stripe.redirectToCheckout({
-            sessionId,
-        });
+    const handleSubmit = async (event) => {
+        event.preventDefault();
 
-        if (result.error) {
-            console.error(result.error);
+        try {
+            const response = await fetch(`http://localhost:3001/api/orders/${cart.id}`, {
+                method: "PATCH",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({
+                    status: "inProgress"
+                })
+            });
+            const translatedData = await response.json();
+            console.log(translatedData);
+        } catch (error) {
+            console.log(error);
         }
     };
 
-    return <button onClick={handleClick}>Checkout</button>;
+    return (
+        <div>
+            <form onSubmit={handleSubmit}>
+                <label>
+                    Card Number:
+                    <input
+                        type="text"
+                        name="cardNumber"
+                        value={formData.cardNumber}
+                        onChange={handleFormChange}
+                    />
+                </label>
+                <label>
+                    Cardholder Name:
+                    <input
+                        type="text"
+                        name="cardName"
+                        value={formData.cardName}
+                        onChange={handleFormChange}
+                    />
+                </label>
+                <label>
+                    Expiration Date:
+                    <input
+                        type="text"
+                        name="expiration"
+                        value={formData.expiration}
+                        onChange={handleFormChange}
+                    />
+                </label>
+                <label>
+                    CVV:
+                    <input
+                        type="text"
+                        name="cvv"
+                        value={formData.cvv}
+                        onChange={handleFormChange}
+                    />
+                </label>
+                <label>
+                    Address:
+                    <input
+                        type="text"
+                        name="address"
+                        value={formData.address}
+                        onChange={handleFormChange}
+                    />
+                </label>
+                <label>
+                    City:
+                    <input
+                        type="text"
+                        name="city"
+                        value={formData.city}
+                        onChange={handleFormChange}
+                    />
+                </label>
+                <label>
+                    State:
+                    <input
+                        type="text"
+                        name="state"
+                        value={formData.state}
+                        onChange={handleFormChange}
+                    />
+                </label>
+                <label>
+                    Zip Code:
+                    <input
+                        type="text"
+                        name="zip"
+                        value={formData.zip}
+                        onChange={handleFormChange}
+                    />
+                </label>
+                <button type="submit">
+                    Checkout
+                </button>
+            </form>
+        </div>
+    );
 };
 
-export default StripeCheckoutButton;
+export default Checkout;
